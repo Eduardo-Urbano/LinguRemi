3c4ec8c5d07dd585db39670fd150dccad907cdb7
Eduardo Urbano <eduardosurbano004@gmail.com>
2025-10-10 17:38:30 -0300

    Correção do "output.css", ajuste do tamanho dos cards, adição da favicon e leve reorganização na estrutura.

--- src/scripts/doces.ts
+++ src/scripts/doces.ts
@@ -6,25 +6,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let index = 0;
 
-function criarCards() {
+    function criarCards() {
     container.innerHTML = "";
 
     for (let i = 0; i < receitasDoce.length; i++) {
         const receita = receitasDoce[i]!;
 
         const card = document.createElement("div");
-        card.className = "bg-white p-4 rounded shadow flex flex-col h-80"; 
-        // h-80 = altura fixa, flex-col para alinhar verticalmente
+        card.className = "bg-white rounded-xl shadow-lg overflow-hidden flex flex-col";
 
         card.innerHTML = `
-            <img src="${receita.imagem}" alt="${receita.nome}" class="w-full h-32 object-cover rounded mb-2">
-            <h3 class="font-bold text-lg">${receita.nome}</h3>
+        <img src="${receita.imagem}" alt="${receita.nome}" class="w-full h-48 object-cover">
+        
+        <div class="p-4 flex flex-col"> 
+            <h3 class="font-bold text-lg mb-2">${receita.nome}</h3> 
             <p class="text-gray-600 text-sm">${receita.descricao}</p>
+        </div>
         `;
 
         container.appendChild(card);
     }
-}
+    }
 
     document.getElementById("next")?.addEventListener("click", () => {
         index = (index + 1) % receitasDoce.length;
@@ -32,8 +34,8 @@ function criarCards() {
     });
 
     document.getElementById("prev")?.addEventListener("click", () => {
-        index = (index - 1 + receitasDoce.length) % receitasDoce.length;
-        criarCards();
+    index = (index - 1 + receitasDoce.length) % receitasDoce.length;
+    criarCards();
     });
 
     criarCards();
