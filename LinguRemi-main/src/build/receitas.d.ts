b8259dc74f386a92eb6cc008c5614b2cc07cfae8
Giovanni França <37194964+Giovanni-Franca@users.noreply.github.com>
2025-10-26 22:10:56 -0300

    Delete LinguRemi-main directory
    upei errado

--- LinguRemi-main/src/build/receitas.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const receitasDoce: {
-    nome: string;
-    descricao: string;
-    imagem: string;
-    preparo: string;
-    preco: string;
-    data: string;
-}[];
-//# sourceMappingURL=receitas.d.ts.map
